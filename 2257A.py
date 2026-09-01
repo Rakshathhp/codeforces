da555fe6fc94dc5cc850289f1d7d7019efae5643
Rakshath Hp <rakshathhp17@gmail.com>
2026-09-01 23:11:59 +0530

    updated sep 1/26

--- 2257A.py
+++ 2257A.py
@@ -1,23 +1,17 @@
-t=int(input())
+t = int(input())
 
 for _ in range(t):
-	n=[]
-	m=[]
-	
-	a,b=map(int,input().split())
-	for i in range(a):
-		x=input()
-		x=x.lower()
-		n.append(x[0])
-	for j in range(b):
-		y=input()
-		y=y.lower()
-		m.append(y[0])
-	c=0
-	for ch in m:
-		if ch in n:
-			c+=1
-			print("yes")
-			break
-	if c==0:
-		print("no")
+    n = set()
+    a, b = map(int, input().split())
+    for i in range(a):
+        x = input().lower()
+        n.add(x[0])
+
+    ok = True
+    for j in range(b):
+        y = input().lower()
+        for ch in y:           
+            if ch not in n:
+                ok = False
+
+    print("yes" if ok else "no")
