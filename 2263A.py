2fb97ae781c7285b2eb57f4f83b0f6b5cbbff284
Rakshath Hp <rakshathhp17@gmail.com>
2026-09-19 23:58:43 +0530

    sep 19/26

---
+++ 2263A.py
@@ -0,0 +1,10 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+   
+
+    if a.count(1) >= a.count(0):
+        print("Bessie")
+    else:
+        print("Elsie")
